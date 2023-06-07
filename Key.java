7331b2edad78f72effd2ff663a757d1d009a6a55
VitovskyiS <114645964+VitovskyiS@users.noreply.github.com>
2023-06-07 18:11:54 +0300

    Minesweeper

---
+++ Key.java
@@ -0,0 +1,16 @@
+package com.example.minesweepergame;
+
+public enum Key {
+    UNKNOWN,
+    ENTER,
+    ESCAPE,
+    SPACE,
+    PAUSE,
+    LEFT,
+    RIGHT,
+    UP,
+    DOWN;
+
+    private Key() {
+    }
+}
